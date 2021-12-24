e566e6dbd3c821d5264bc7effc162df85857400f
kraftytek <61486981+kraftytek@users.noreply.github.com>
2021-12-23 19:29:17 -0800

    Update kraftBotDataCol.py
    added sig and removed connection info from script.

--- kraftBotDataCol.py
+++ kraftBotDataCol.py
@@ -15,24 +15,25 @@ with open("prawpages.txt", "r") as p:
 #prawkey configured same order as connection bellow with each being on a new line 
 with open("prawkey.txt", "r") as f:
     lines = f.read().split('\n')
-    
-#with open("sqlConnect.txt", "r") as g:
-#    sqlLines = g.read().split('\n')
 
-#create a function to perform the scrape
-      
+#provides SQL creds to access the specifed DB    
+with open("sqlConnect.txt", "r") as g:
+    sqlLines = g.read().split('\n')
+
+#create a function to perform the scrape      
 def scrapeToDB():
     
     reddit = praw.Reddit(
         client_id=lines[0],
         client_secret=lines[1],
-        user_agent=lines[2],
+        user_agent=lines[2]
     )
-     
-    conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=sql.kraftytek.ca;DATABASE=ApiDump;UID=sa;PWD=S!lver88')
-    cursor = conn.cursor()    
+    dbConnect = sqlLines[0]     
+    conn = pyodbc.connect(dbConnect)
+    cursor = conn.cursor() 
     
-    for submission in reddit.subreddit(pageList[pageNum]).hot(limit=1000):        
+#dump post titles into db with time stamps   
+    for submission in reddit.subreddit(pageList[pageNum]).hot(limit=1000):       
         
         textString = "[" + submission.title + "]"        
         query = "insert into dataDumper(dataString, datestamp) values(?, getdate())"    
@@ -46,5 +47,12 @@ for i in pageList:
     scrapeToDB()
     time.sleep(3)
     pageNum += 1   
+
+print("  ___________   ")
+print(" /           \  ")
+print(" \  Made By  /  ")
+print(" / Kraftytek \  ")
+print(" \___________/  ")   
+
 
         
